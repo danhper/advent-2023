68b982ba4c8e6b2d8178c93a9185f6bb43ebee6a
Daniel Perez <daniel@perez.sh>
2023-12-01 20:46:46 +0000

    Refactor day 1

--- src/App/Day1.fs
+++ src/App/Day1.fs
@@ -3,28 +3,27 @@ module Day1
 let digits =
     [ "one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine" ]
 
-let mapped_digits = List.mapi (fun i v -> ((string (i + 1)), v)) digits
 
-let get_digit index_of extract_by line parse =
-    let get_value v c =
-        (v, index_of line (string v))
-        :: if parse then [ (v, index_of line (string c)) ] else []
+let next_indices current first last =
+    match current, first with
+    | (None, _) -> (first, last)
+    | (Some _) as c, ((Some _) as f) -> (f, c)
+    | (Some _) as c, None -> (c, c)
 
-    let values = List.collect (fun (v, c) -> get_value v c) mapped_digits
-    List.filter (fun (_, i) -> i <> -1) values |> extract_by snd |> fst
+let starts_with_number parse (line: string) number =
+    line.StartsWith(string number) || (parse && line.StartsWith(digits[number - 1]))
 
-let extract_number parse (line: string) =
-    let first_digit =
-        get_digit (fun (s1: string) s2 -> s1.IndexOf(s2)) List.minBy line parse
+let compute_next parse line (first, last) =
+    let current = Seq.tryFind (starts_with_number parse line) { 1..9 }
+    next_indices current first last
 
-    let last_digit =
-        get_digit (fun (s1: string) s2 -> s1.LastIndexOf(s2)) List.maxBy line parse
-
-    int (first_digit + last_digit)
-
+let get_number parse (line: string) =
+    let folder acc i = compute_next parse line[i..] acc
+    let (first, last) = Seq.fold folder (None, None) { 0 .. line.Length - 1 }
+    Option.get first * 10 + Option.get last
 
 let solve lines parse =
-    lines |> List.map (extract_number parse) |> List.sum
+    lines |> List.map (get_number parse) |> List.sum
 
 let run suffix =
     let lines = Utils.get_file_name 1 suffix |> Utils.read_lines
